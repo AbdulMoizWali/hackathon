24cfcff632b403b2b5832bf424e18e81b830c486
AbdulMoizWali <85098925+AbdulMoizWali@users.noreply.github.com>
2023-05-14 12:34:37 +0500

    Card component

---
+++ src/Components/SMCard.js
@@ -0,0 +1,8 @@
+import { Card } from "@mui/material";
+
+export default function SMCard() {
+    <Card
+        variant="outlined"
+        sx={{ display: 'inline-block', mx: '2px', transform: 'scale(1)', px: '30px', py: '60px' }}
+    ></Card>
+}
